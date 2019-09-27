b106348c86a9ca863e28f5a10cdf4794903cbf60
ijungleboy <daniel.mettler@2sic.com>
2019-09-27 08:21:21 +0200

    added round images of people

--- ng/src/app/person/person.component.ts
+++ ng/src/app/person/person.component.ts
@@ -2,7 +2,8 @@ import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'person',
-  templateUrl: './person.component.html'
+  templateUrl: './person.component.html',
+  styleUrls: ['./person.component.scss']
 })
 export class PersonComponent {
   @Input() person: any;
